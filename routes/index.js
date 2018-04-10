e90f8749f9d75819adf70ef179e426c98e56cfbd
Kenya <email@email.com>
2018-04-10 18:36:12 -0500

    astropix lab

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var apodService = require('../services/apod');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('fetchpicture');
+});
+
+/* Fetch a picture from NASA's Astronomy Picture of the Day service */
+router.get('/fetchpicture', function(req, res, next){
+
+  // If random picture requested, fetch random picture.
+  // Otherwise fetch today's picture
+  apodService(function(err, apod_data){
+    if (err) {
+      res.render('apod_error', { message: err.message, title: 'Error'});
+    } else {
+      res.render('index', {apod: apod_data, title: `APOD for ${apod_data.date}`});
+    }
+  }, req.query.picturetype);
 });
 
 module.exports = router;
